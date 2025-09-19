422b11810c11dc51e2a083f07697145b5adac9b9
avtzone <avtzone1@gmail.com>
2025-09-19 19:31:39 +0530

    Update auto-sticker.js

--- plugins/auto-sticker.js
+++ plugins/auto-sticker.js
@@ -20,7 +20,7 @@ async (conn, mek, m, { from, body }) => {
 
                     await conn.sendMessage(from, {
                         sticker: stickerBuffer,
-                        packname: 'JESTER-AI',
+                        packname: 'Viruna-AI',
                         author: 'AUTO-STICKER'
                     }, { quoted: mek });
                 } else {
