710013b5d22a358ea34ec91c6e553be8ccf8cde6
avtzone <avtzone1@gmail.com>
2025-09-19 20:04:26 +0530

    Update fetch-quote.js

--- plugins/fetch-quote.js
+++ plugins/fetch-quote.js
@@ -12,7 +12,7 @@ cmd({
     const response = await axios.get("https://api.quotable.io/random");
     const { content, author } = response.data;
 
-    const message = `💬 *"${content}"*\n- ${author}\n\n> *QUOTES BY AV-Tech MD*`;
+    const message = `💬 *"${content}"*\n- ${author}\n\n> *Viruna MD*`;
     reply(message);
   } catch (error) {
     console.error("Error fetching quote:", error);
