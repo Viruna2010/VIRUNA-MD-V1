d31d44ba941fcdc7bf6058ae1306aaafb98c8a1e
avtzone <avtzone1@gmail.com>
2025-09-20 14:11:13 +0530

    Update img-grey.js

--- plugins/img-grey.js
+++ plugins/img-grey.js
@@ -75,7 +75,7 @@ cmd({
 
     await conn.sendMessage(m.chat, {
       image: imageBuffer,
-      caption: `> *Powered by JesterTechX*`
+      caption: `> *Powered by Viruna MD*`
     });
 
   } catch (error) {
