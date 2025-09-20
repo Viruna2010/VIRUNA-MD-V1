82662c394c5b55765f2b8324655870b7175203a9
avtzone <avtzone1@gmail.com>
2025-09-20 14:25:20 +0530

    Update img-wanted.js

--- plugins/img-wanted.js
+++ plugins/img-wanted.js
@@ -75,7 +75,7 @@ cmd({
 
     await conn.sendMessage(m.chat, {
       image: imageBuffer,
-      caption: `> *Powered by David X*`
+      caption: `> *Powered by Viruna MD*`
     });
 
   } catch (error) {
