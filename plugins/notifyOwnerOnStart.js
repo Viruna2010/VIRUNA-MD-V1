f56ab8412df38609101e2f192565423191820541
avtzone <avtzone1@gmail.com>
2025-09-26 15:40:27 +0530

    Update notifyOwnerOnStart.js

--- plugins/notifyOwnerOnStart.js
+++ plugins/notifyOwnerOnStart.js
@@ -1,15 +1,11 @@
 module.exports = (conn) => {
-    const ownerNumber = ['94761138211'] // Owner number
-
-    conn.ev.on('connection.update', async (update) => {
-        const { connection } = update
-
-        if (connection === 'open') {
-            for (let num of ownerNumber) {
-                await conn.sendMessage(num + "@s.whatsapp.net", {
-                    text: `✅ Bot connected successfully! Hello Owner ( ${num} ), your Viruna-MD bot is now online 🚀`
-                })
-            }
-        }
-    })
+  const ownerNumbers = ['94761138211'];
+  conn.ev.on('connection.update', async (update) => {
+    const { connection } = update;
+    if (connection === 'open') {
+      for (let num of ownerNumbers) {
+        await conn.sendMessage(num + "@s.whatsapp.net", { text: `✅ Bot connected successfully!` });
+      }
+    }
+  });
 }
